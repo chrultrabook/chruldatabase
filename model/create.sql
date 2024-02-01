8c0812432ad9e33f393ebad5d3b6469d41e17ea6
ninelore <ninelore@protonmail.com>
2024-02-01 12:43:15 +0100

    quarkus init commit

--- model/create.sql
+++ model/create.sql
@@ -7,54 +7,130 @@ CREATE TYPE cbdb.os AS ENUM ('windows', 'linux', 'macos');
 
 CREATE TABLE IF NOT EXISTS cbdb.brand
 (
-    id   int NOT NULL UNIQUE,
-    name varchar,
-    primary key (id)
-);
+    id
+    int
+    NOT
+    NULL
+    UNIQUE,
+    name
+    varchar,
+    primary
+    key
+(
+    id
+)
+    );
 
 CREATE TABLE IF NOT EXISTS cbdb.device
 (
-    id       int     NOT NULL UNIQUE,
-    name     varchar NOT NULL,
-    arch_id  int,
-    brand_id int,
-    primary key (id)
-);
+    id
+    int
+    NOT
+    NULL
+    UNIQUE,
+    name
+    varchar
+    NOT
+    NULL,
+    arch_id
+    int,
+    brand_id
+    int,
+    primary
+    key
+(
+    id
+)
+    );
 
 CREATE TABLE IF NOT EXISTS cbdb.device_notes
 (
-    id        int     NOT NULL UNIQUE,
-    os        cbdb.os NOT NULL,
-    device_id int,
-    note      varchar,
-    primary key (id)
-);
+    id
+    int
+    NOT
+    NULL
+    UNIQUE,
+    os
+    cbdb
+    .
+    os
+    NOT
+    NULL,
+    device_id
+    int,
+    note
+    varchar,
+    primary
+    key
+(
+    id
+)
+    );
 
 CREATE TABLE IF NOT EXISTS cbdb.gen
 (
-    id                    int NOT NULL UNIQUE,
-    short                 varchar(3),
-    name                  varchar,
+    id
+    int
+    NOT
+    NULL
+    UNIQUE,
+    short
+    varchar
+(
+    3
+),
+    name varchar,
     stock_kernel_partsize int,
-    primary key (id)
-);
+    primary key
+(
+    id
+)
+    );
 
 CREATE TABLE IF NOT EXISTS cbdb.gen_notes
 (
-    id               int     NOT NULL UNIQUE,
-    os               cbdb.os NOT NULL,
-    arch_id          int,
-    note             varchar,
-    show_on_dev_note bool,
-    primary key (id)
-);
+    id
+    int
+    NOT
+    NULL
+    UNIQUE,
+    os
+    cbdb
+    .
+    os
+    NOT
+    NULL,
+    arch_id
+    int,
+    note
+    varchar,
+    show_on_dev_note
+    bool,
+    primary
+    key
+(
+    id
+)
+    );
 
 CREATE TABLE IF NOT EXISTS cbdb.win_drivers
 (
-    id            int NOT NULL UNIQUE,
-    device_id     int,
-    arch_id       int,
-    name          varchar,
-    download_link varchar,
-    primary key (id)
-);
+    id
+    int
+    NOT
+    NULL
+    UNIQUE,
+    device_id
+    int,
+    arch_id
+    int,
+    name
+    varchar,
+    download_link
+    varchar,
+    primary
+    key
+(
+    id
+)
+    );
