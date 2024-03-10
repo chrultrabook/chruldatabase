b53cfdc9e94b9f2ddce2f62e8c9e3bc9f5ab6632
ninelore <9l@9lo.re>
2024-03-10 09:57:55 +0100

    REST endpoints

--- src/main/java/com/chrultrabook/cbdb/rest/EntityMapper.java
+++ src/main/java/com/chrultrabook/cbdb/rest/EntityMapper.java
@@ -8,35 +8,37 @@ import com.chrultrabook.cbdb.rest.bean.Device;
 import com.chrultrabook.cbdb.rest.bean.DeviceNote;
 import com.chrultrabook.cbdb.rest.bean.Generation;
 import com.chrultrabook.cbdb.rest.bean.GenerationNote;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
+@ApplicationScoped
 public class EntityMapper {
 
-    public Generation getAndMapGenerationWithDevices(String shortName) {
-        Optional<GenerationEntity> genEntityOpt = GenerationEntity.find("shortName", shortName).firstResultOptional();
-        if (genEntityOpt.isEmpty()) return null;
-        List<GenerationNote> notes = getNotesForGeneration(genEntityOpt.get().id);
-        List<DeviceEntity> deviceEntities = DeviceEntity.list("generationId", genEntityOpt.get().id);
-        List<Device> devices = deviceEntities.parallelStream().map(i -> getAndMapDevice(i.boardName)).toList();
-        return genEntityOpt.get().toRecord(devices, notes);
+    public List<Generation> getEverything() {
+        List<GenerationEntity> generationEntityList = GenerationEntity.listAll();
+        return generationEntityList.parallelStream().map(this::getAndMapGenerationWithDevices).toList();
     }
 
-    public List<GenerationNote> getNotesForGeneration(int generationId) {
+    public Generation getAndMapGenerationWithDevices(GenerationEntity gen) {
+        List<GenerationNote> notes = getNotesForGeneration(gen.id);
+        List<DeviceEntity> deviceEntities = DeviceEntity.list("generationId", gen.id);
+        List<Device> devices = deviceEntities.parallelStream().map(this::getAndMapDevice).toList();
+        return gen.toRecord(devices, notes);
+    }
+
+    private List<GenerationNote> getNotesForGeneration(int generationId) {
         List<GenerationNoteEntity> notes = GenerationNoteEntity.list("generationId", generationId);
         return notes.parallelStream().map(GenerationNoteEntity::toRecord).collect(Collectors.toList());
     }
 
-    public Device getAndMapDevice(String boardName) {
-        Optional<DeviceEntity> deviceEntityOpt = DeviceEntity.find("boardName", boardName).firstResultOptional();
-        if (deviceEntityOpt.isEmpty()) return null;
-        List<DeviceNote> notes = getNotesForDevice(deviceEntityOpt.get().id);
-        return deviceEntityOpt.get().toRecord(notes);
+    public Device getAndMapDevice(DeviceEntity device) {
+        List<DeviceNote> notes = getNotesForDevice(device.id);
+        return device.toRecord(notes);
     }
 
-    public List<DeviceNote> getNotesForDevice(int deviceId) {
+    private List<DeviceNote> getNotesForDevice(int deviceId) {
         List<DeviceNoteEntity> notes = DeviceNoteEntity.list("deviceId", deviceId);
         return notes.parallelStream().map(DeviceNoteEntity::toRecord).collect(Collectors.toList());
     }
--- src/main/java/com/chrultrabook/cbdb/rest/RS.java
+++ src/main/java/com/chrultrabook/cbdb/rest/RS.java
@@ -1,4 +1,51 @@
 package com.chrultrabook.cbdb.rest;
 
+import com.chrultrabook.cbdb.entity.DeviceEntity;
+import com.chrultrabook.cbdb.entity.GenerationEntity;
+import com.chrultrabook.cbdb.rest.bean.Generation;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+import java.util.Optional;
+
+@Path("v1")
 public class RS {
+
+    @Inject
+    EntityMapper em;
+
+    @GET
+    @Produces("application/json")
+    @Path("everything")
+    public List<Generation> getAll() {
+        return em.getEverything();
+    }
+
+    @GET
+    @Produces("application/json")
+    @Path("generation/{shortName}")
+    public Response getGeneration(@PathParam("shortName") String shortName) {
+        Optional<GenerationEntity> gen = GenerationEntity.find("shortName", shortName).firstResultOptional();
+        if (gen.isPresent()) {
+            return Response.ok(em.getAndMapGenerationWithDevices(gen.get())).build();
+        }
+        return Response.status(404, "Generation data not found").build();
+    }
+
+    @GET
+    @Produces("application/json")
+    @Path("device/{boardName}")
+    public Response getDevice(@PathParam("boardName") String boardName) {
+        Optional<DeviceEntity> device = DeviceEntity.find("boardName", boardName).firstResultOptional();
+        if (device.isPresent()) {
+            return Response.ok(em.getAndMapDevice(device.get())).build();
+        }
+        return Response.status(404, "Device data not found").build();
+    }
+
 }
