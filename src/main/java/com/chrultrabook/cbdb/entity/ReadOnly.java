2d68777620a037a2bdd4f8f6091f4aedb0fd0822
Ingo Reitz <9l@9lo.re>
2024-08-25 22:09:47 +0200

    fix nosuchmethodexception in quarkus native

--- src/main/java/com/chrultrabook/cbdb/entity/ReadOnly.java
+++ src/main/java/com/chrultrabook/cbdb/entity/ReadOnly.java
@@ -1,23 +1,25 @@
 package com.chrultrabook.cbdb.entity;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreRemove;
 import jakarta.persistence.PreUpdate;
 
+@RegisterForReflection
 public class ReadOnly {
 
     @PrePersist
-    static void onPrePersist(Object o) {
+    void onPrePersist(Object o) {
         throw new IllegalStateException("JPA is trying to persist an entity of type " + (o == null ? "null" : o.getClass()));
     }
 
     @PreUpdate
-    static void onPreUpdate(Object o) {
+    void onPreUpdate(Object o) {
         throw new IllegalStateException("JPA is trying to update an entity of type " + (o == null ? "null" : o.getClass()));
     }
 
     @PreRemove
-    static void onPreRemove(Object o) {
+    void onPreRemove(Object o) {
         throw new IllegalStateException("JPA is trying to remove an entity of type " + (o == null ? "null" : o.getClass()));
     }
 
